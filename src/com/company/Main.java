4a13e325b7b406ad80327ef2053b721dd2f2c875
Overlord7667 <aspiaro@gmail.com>
2023-03-08 21:14:19 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,107 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Enter a = ");
+//        int a = scanner.nextInt();
+//
+//        for (int i = 0; i < a; i++) {
+//            for (int b = 0; b < a; b++) {
+//                System.out.print(" * ");
+//            }
+//            System.out.println();
+//        }
+
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Enter a = ");
+//        int a = scanner.nextInt();
+//
+//        for (int i = 0; i < a; i++){
+//            for (int j = i; j < a; j++){
+//                System.out.print("*");
+//            }
+//            System.out.println("");
+//        }
+
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Enter a = ");
+//        int a = scanner.nextInt();
+//
+//        for (int i = a; i >= 0; i--){
+//            for (int j = i; j <= a; j++){
+//                System.out.print("* ");
+//            }
+//            System.out.println("");
+//        }
+
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Enter a = ");
+//        int a = scanner.nextInt();
+//        for (int i = 0; i <= a; i++){
+//
+//            for (int j = a; j > i; j--){
+//                System.out.print(" ");
+//            }
+//
+//            for (int j1 = 0; j1 <= i; j1++){
+//                System.out.print("*");
+//            }
+//
+//            for (int j = 0; j < i; j++){
+//                System.out.print("*");
+//            }
+//
+//            for (int j = a; j > i; j--){
+//                System.out.print(" ");
+//            }
+//
+//            System.out.println("");
+//        }
+
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Enter a = ");
+//        int a = scanner.nextInt();
+//        for (int i = 0; i < a; i++){
+//            //Левое пустое пространство
+//            for (int j = a; j > i; j--){
+//                System.out.print(" ");
+//            }
+//            //Левая часть треугольника
+//            for (int j1 = 0; j1 <= i; j1++){
+//                System.out.print("*");
+//            }
+//            //Правая часть треугольника
+//            for (int j = 0; j < i; j++){
+//                System.out.print("*");
+//            }
+//            //Правое пустое пространство
+//            for (int j = a; j > i; j--){
+//                System.out.print(" ");
+//            }
+//            System.out.println(" ");
+//        }
+//        for (int i = a; i >= 0; i--){
+//            //Левое пустое пространство
+//            for (int j = a; j > i; j--){
+//                System.out.print(" ");
+//            }
+//            //Левая часть треугольника
+//            for (int j1 = 0; j1 <= i; j1++){
+//                System.out.print("*");
+//            }
+//            //Правая часть треугольника
+//            for (int j = 0; j < i; j++){
+//                System.out.print("*");
+//            }
+//            //Правое пустое пространство
+//            for (int j = a; j > i; j--){
+//                System.out.print(" ");
+//            }
+//            System.out.println(" ");
+//        }
+    }
+}
